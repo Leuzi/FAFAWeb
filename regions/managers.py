a314caa31dbc19c569c00d290281ed6c8aa7f4dd
Ramon Izuel Rios <izuelrios@gmail.com>
2017-01-27 18:47:22 +0100

    added new manager for regions

---
+++ regions/managers.py
@@ -0,0 +1,19 @@
+from managers import models
+from FAFAWeb.constants import *
+
+class RegionManager():
+
+	@classmethods
+	def getAllRegions(self):
+		regions = models.Region.all()
+		
+		if regions == []:
+			regions = None
+			
+		return regions
+	
+	def getRegionByShortname(self,shortname):
+		
+		return models.Region.objects.get(ShortRegionName=shortname)
+		
+		
